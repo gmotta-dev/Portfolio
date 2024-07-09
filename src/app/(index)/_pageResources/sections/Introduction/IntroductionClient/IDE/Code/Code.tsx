1b361fc048e7c1cec269907fc161bbf1ffcae51d
Gabriel Pacheco Motta <gpmotta21@gmail.com>
2024-07-08 22:12:02 -0300

    :zap: Remove JS Highlight for tests

--- src/app/(index)/_pageResources/sections/Introduction/IntroductionClient/IDE/Code/Code.tsx
+++ src/app/(index)/_pageResources/sections/Introduction/IntroductionClient/IDE/Code/Code.tsx
@@ -1,25 +1,11 @@
 import Markdown from "markdown-to-jsx";
 import React, { useEffect, useRef, useState } from "react";
-import hljs from "highlight.js";
-import "./pojoaque.css";
 import { twMerge } from "tailwind-merge";
 import { TIDECurrView } from "../config";
 import { AnimatePresence, motion } from "framer-motion";
 
 export default function Code(props: TIDECurrView) {
-  const [show, setShow] = useState(false);
   const contentRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (contentRef.current) {
-      const codeBlocks = contentRef.current.querySelectorAll("pre code");
-      codeBlocks.forEach((block) => {
-        hljs.highlightElement(block as HTMLElement);
-      });
-    }
-
-    setShow(true);
-  }, [props.tab.label, props.previewContent]);
 
   return (
     <div ref={contentRef} key={`${props.tab.label}-${props.previewContent}-div`} className="h-[392px] w-full overflow-scroll bg-neutral-950 p-4">
@@ -29,8 +15,8 @@ export default function Code(props: TIDECurrView) {
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 100 }}
           transition={{ duration: 1, type: "spring", damping: 10, stiffness: 100 }}
-          className={twMerge("transition-opacity duration-[800ms] relative pl-8", show ? "opacity-100" : "opacity-0")}>
-          <ul className="flex flex-col absolute left-0 top-[1px] text-neutral-700">
+          className={twMerge("relative pl-8 transition-opacity duration-[800ms]")}>
+          <ul className="absolute left-0 top-[1px] flex flex-col text-neutral-700">
             {[...new Array(22)].map((_, index) => (
               <li key={index}>{index + 1}</li>
             ))}
--- src/app/(index)/_pageResources/sections/Introduction/IntroductionClient/IDE/IDE.tsx
+++ src/app/(index)/_pageResources/sections/Introduction/IntroductionClient/IDE/IDE.tsx
@@ -1,17 +1,16 @@
 import Bottom from "./Bottom";
+import Code from "./Code/Code";
 import { TIDECurrView } from "./config";
 import Tabs from "./Tabs";
 import Upper from "./Upper";
-import dynamic from "next/dynamic";
 
-const Content = dynamic(() => import("./Code/Code"));
 
 export default function IDE(props: { view: TIDECurrView; setView: (view: TIDECurrView) => void }) {
   return (
     <div className="flex max-h-[468px] w-full flex-col border border-neutral-700 lg:max-w-[597px]">
       <Upper />
       <Tabs {...props} />
-      <Content {...props.view} />
+      <Code {...props.view} />
       <Bottom />
     </div>
   );
